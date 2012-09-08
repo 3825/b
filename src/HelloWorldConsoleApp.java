e6c7c104d9d3abdeb11079ea53c6d4837718bda1
Kushal <kforkansas@gmail.com>
2012-09-08 17:14:23 -0500

    Hello world

---
+++ src/HelloWorldConsoleApp.java
@@ -0,0 +1,10 @@
+/**
+ * User: Ku
+ * Date: 9/8/12
+ * Time: 5:02 PM
+ */
+ class HelloWorldConsoleApp {
+    public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    }
+}
