9c828b31181f4ac127ef4535fe578fef7a0f06ad
Kushal <kforkansas@gmail.com>
2012-09-09 16:07:50 -0500

    Added a readme file

--- src/HelloWorldConsoleApp.java
+++ src/HelloWorldConsoleApp.java
@@ -5,6 +5,6 @@
  */
  class HelloWorldConsoleApp {
     public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world! How are you today?");
     }
 }
