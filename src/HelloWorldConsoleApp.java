148710a0fbcc1c6fa374af19cafc7b210c6c63dc
Kushal <kforkansas@gmail.com>
2012-09-12 07:58:30 -0500

    Today's date

--- src/HelloWorldConsoleApp.java
+++ src/HelloWorldConsoleApp.java
@@ -5,6 +5,6 @@
  */
  class HelloWorldConsoleApp {
     public static void main(String[] args) {
-    System.out.println("Hello, world! How are you today?");
+    System.out.println("Hello, Denver! Today is September 12, 2012. It is a brand new day.");
     }
 }
